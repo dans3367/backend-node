a1ea02e040a4d28b8cf46c19b58d705d51c0e108
dans3367 <dans3367@gmail.com>
2023-11-22 03:56:58 -0600

    updated resolvers remove duplictae mutation

--- src/graphql/campaign/resolvers.js
+++ src/graphql/campaign/resolvers.js
@@ -3,58 +3,17 @@ import { campaign } from "../../models/campaign.js";
 import { validate } from "../../validation/campaign.js";
 
 export const CampaignResolver = {
-    Query: {
-        //campaigns: async () => await campaign.find(),
-        campaignList: async (_, { page, perPage }) => {
-
-          return await campaign.paginate({},page,perPage);
-
-        }
-
-        
-    },
-
-    Mutation: {
-        addCampaign: async (root, args, { prisma }, info) => {
-          const {data} = args;
-
-          const errors = validate(data)
-
-          if(errors) return {
-
-          }
-          
-          const newCampaign = {
-            name: data.name,
-            title: data.title,
-            description: data.description || null,
-            effective_dates: {
-              startDate: data.effective_dates.startDate,
-              endDate: data.effective_dates.endDate,
-            },
-            delivery_method: {
-              email: data.delivery_method.email || false,
-              phone: data.delivery_method.phone || false,
-            },
-            stores: data.stores,
-            tags: data.tags,
-          };
-    
-          const response = await campaign.create(newCampaign);
-
-      return await campaign.deleteById(campaignId);
-
+  Query: {
+    campaignList: async (_, { page, perPage }) => {
+      return await campaign.paginate({}, page, perPage);
     },
   },
 
   Mutation: {
-    addCampaign: async (_, { data },payload) => {
-      
-      const errors = validate(data)
+    addCampaign: async (_, { data }, payload) => {
+      const errors = validate(data);
 
-      if (errors) return null
-
-     // console.log('payload',payload)
+      if (errors) return null;
 
       const newCampaign = {
         company_id: payload.company_id,
@@ -66,13 +25,25 @@ export const CampaignResolver = {
           startDate: data.effective_dates.startDate,
           endDate: data.effective_dates.endDate,
         },
+        delivery_method: {
+          email: data.delivery_method.email || false,
+          phone: data.delivery_method.phone || false,
+        },
+        stores: data.stores,
+        tags: data.tags,
+      };
 
-        updateCampaign: async (_, { _id, input }) => {
-          
-          const errors = validate(input)
+      const response = await campaign.create(newCampaign);
 
-      return newCampaign;
+      return await campaign.deleteById(campaignId);
     },
 
+    updateCampaign: async (_, { _id, input }) => {
+      const errors = validate(input);
 
-  };
+      // Add your logic for updating the campaign using _id and input
+
+      return updatedCampaign; // You need to implement the logic for updatedCampaign
+    },
+  },
+};
